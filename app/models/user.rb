937ce59f9cbd33aaf72b9c83a5e6f54cb50e9906
ryr11 <ryrams111@gmail.com>
2014-02-26 18:00:59 -0800

    Add User form and profile pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
 	has_secure_password
 	validates :name, presence: true
 	validates :username, uniqueness: true, presence: true
-	validates :email, uniqueness: true, presence: true, format: { with: /^[\w.+-]+@([\w]+.)+\w+$/ }
+	validates :email, uniqueness: true, presence: true, format: { with: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i }
 	private
 	def prep_email
     	self.email = self.email.strip.downcase if self.email
