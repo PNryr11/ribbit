5f1ce9d828d7d6fbd64b7958a3366a8263f9b7a8
ryr11 <ryrams111@gmail.com>
2014-02-26 18:12:15 -0800

    users are now logged in upon creation

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_username(params[:username])
+    if user && user.authenticate(params[:password])
+        session[:userid] = user.id
+        redirect_to rooturl, notice: "Logged in!"
+    else
+        flash[:error] = "Wrong Username or Password."
+        redirect_to root_url
+    end
+	end
+
+	def destroy
+    	session[:userid] = nil
+    	redirect_to root_url, notice: "Logged out."
+	end
+	private
+ 
+	def current_user
+    	@current_user ||= User.find(session[:user_id]) if session[:user_id]
+	end
+ 
+	helper_method :current_user
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Ribbit::Application.routes.draw do
+  get "sessions/new"
+  post "sessions/create"
+  get "sessions/destroy"
   resources :users
   root to: 'users#new'
 
